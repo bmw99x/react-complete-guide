7edf510af8fe86fc34191dc762d828af8b7ef53c
Ben Whatling <ben.whatling@eigentech.com>
2020-12-30 18:36:08 +0000

    End of assignment 1 chapter

--- src/App.js
+++ src/App.js
@@ -1,80 +1,105 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '1', name: "Max", age: 28 },
+      { id: '2', name: "Manu", age: 29 },
+      { id: '3', name: "Stephanie", age: 26 },
     ],
-    otherState: 'some other value',
+    otherState: "some other value",
     showPersons: false,
-  }
+  };
 
   switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState( {
+    this.setState({
       persons: [
         { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    } )
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event, id) => {
+    //  Get the personIndex at which the person is equal
+    // to the person with the provided id
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // To avoid mutating state, we use spread operator
+    const person = {...this.state.persons[personIndex]}
+    person.name = event.target.value
+    const persons = [...this.state.persons]
+    persons[personIndex] = person;
+    this.setState({persons: persons})
+  };
+
+  deletePersonsHandler = (personIndex) => {
+    // We shouldn't update state using byVal this.state.persons
+    // instead, we slice().
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
-  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     // Not operator : !
-    this.setState({showPersons: !doesShow});
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
+
+  render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
     };
 
+    let persons = null;
+    // Implicitly check if showPersons===true
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {/* Map every single element in persons array with an arrow function
+           */}
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonsHandler(index)}
+                name={person.name}
+                age={person.age}
+                // to prevent React from re-rendering elements
+                // We can cache these but we need a key.
+                // Use something unique to define key
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
-          {/* Ternary expression to render if showPersons=True */}
-        {this.state.showPersons === true ?
-          <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age} />
-              {/* Else, render nothing / do nothing */}
-        </div> : null
-        }
+        <button style={style} onClick={() => this.togglePersonsHandler()}>
+          Toggle Persons
+        </button>
+        {/* Ternary expression to render if showPersons=True */}
+        {/* Output persons only if showPersons===true */}
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,13 @@
 import React from 'react';
+
 import './Person.css';
-// props is all the properties for the component
-const person = (props) => {
-    // Return some jsx!
+
+const person = ( props ) => {
     return (
         <div className="Person">
-            {/* Add an onClick to the actual functional component <p>*/}
-            <p onClick = {props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            {/* When the value input changes,  */}
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
