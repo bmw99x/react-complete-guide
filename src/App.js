ffec107c812c0b6a2fca174761a613dd9d8ee61a
Ben Whatling <ben.whatling@eigentech.com>
2020-12-30 12:36:51 +0000

    Added ternary operator / togglePersonsHandler

--- src/App.js
+++ src/App.js
@@ -1,63 +1,83 @@
-import React, { Component } from "react";
-import "./App.css";
-import Person from "./Person/Person";
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
-    otherState: "some other value",
-  };
+    otherState: 'some other value',
+    showPersons: false,
+  }
 
   switchNameHandler = (newName) => {
-    // We should not change the states using
-    // this.state.persons[0].name = ''
-    // This will only set the state for persons
-    // not for otherstate
-    this.setState({
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
       persons: [
-        { name: newName, age: 25 },
-        { name: "Manu", age: 29 },
-      ],
-    });
-    console.log("Was clicked");
-  };
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
 
   nameChangedHandler = (event) => {
-    this.setState({
+    this.setState( {
       persons: [
-        { name: event.target.value, age: 42 },
-        { name: "Max", age: 25 },
-        // Assign a new name
-      ],
-    });
-  };
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
 
-  render() {
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    // Not operator : !
+    this.setState({showPersons: !doesShow});
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        {/* onClick to show this is a react button not Javascript*/}
-        <button onClick={this.switchNameHandler.bind(this, "Maximilian")}>
-          Switch Name
-        </button>
-        <Person
-          name="Ben"
-          age="21"
-          click={this.switchNameHandler.bind(this, "Max!")}
-          changed={this.nameChangedHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Bound click")}
-          changed={this.nameChangedHandler}
-        />
+        <button 
+          style={style}
+          onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
+          {/* Ternary expression to render if showPersons=True */}
+        {this.state.showPersons === true ?
+          <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age} />
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Max!')}
+              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+            <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age} />
+              {/* Else, render nothing / do nothing */}
+        </div> : null
+        }
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
